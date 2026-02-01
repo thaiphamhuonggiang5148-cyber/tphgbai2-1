1493eda83d0e6f652bfb73e7e87d118e01c0ec5b
thaiphamhuonggiang5148-cyber <thaiphamhuonggiang5148@gmail.com>
2026-02-01 16:03:04 +0700

    fix: remove duplicate Welcome component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Welcome from './Welcome'
+import Welcome from './Welcome' 
+
 function App() {
   return (
     <>
       <div className='card-title'>
          <Welcome name = "26SPIT3"/>
          <p>Họ tên: Thái Phạm Hương Giang</p>
-         <p>Sở thích: Ngủ,nấu ăn</p>
+         <p>Sở thích: Ngủ, nấu ăn</p>
       </div>
     </>
   )
 }
+
 export default App
