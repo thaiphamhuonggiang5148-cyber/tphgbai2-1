c8b7363662509d5574a2e5ff23327ce972f239bf
thaiphamhuonggiang5148-cyber <thaiphamhuonggiang5148@gmail.com>
2026-02-01 16:58:38 +0700

    4

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import Student from './Student'
 function App() {
   return (
     <div className="container-hocsinh">
-      <Student  name="Thái Phạm Hương Giang" age={17} className="26.SP.IT3" 
-      />
-      <Student name="Lê Thành Được" age={20} className="24CDTKW01" 
-      />
-      <Student name="Trịnh Thái Nguyên" age={19} className="26.SP.IT3" 
-      />
+      <Student  name="Thái Phạm Hương Giang" age={17} className="26.SP.IT3"/>
+      <Student name="Lê Thành Được" age={20} className="24CDTKW01"/>
+      <Student name="Trịnh Thái Nguyên" age={19} className="26.SP.IT3" />
     </div>
   )
 }
