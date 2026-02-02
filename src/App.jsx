2cbc580d50c22c2294d3c0131b7d65e5e10df3a8
thaiphamhuonggiang5148-cyber <thaiphamhuonggiang5148@gmail.com>
2026-02-02 08:31:11 +0700

    add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Student from './Student'
 
 function App() {
   return (
-    <div className="container-hocsinh">
-      <Student  name="Thái Phạm Hương Giang" age={17} className="26.SP.IT3"/>
-      <Student name="Lê Thành Được" age={20} className="24CDTKW01"/>
-      <Student name="Trịnh Thái Nguyên" age={19} className="26.SP.IT3" />
+    <div className="wrapper">
+      <h1 className="main-title">Danh sách học sinh</h1>
+      
+      <div className="container-hocsinh">
+        <Student name="Thái Phạm Hương Giang" age={17} className="26.SP.IT3"/>
+        <Student name="Lê Thành Được" age={20} className="24CDTKW01"/>
+        <Student name="Trịnh Thái Nguyên" age={19} className="26.SP.IT3" />
+      </div>
     </div>
   )
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,11 +1,7 @@
+import './Student.css';
 function Student(props) {
   return (
-    <div style={{
-      border: "1px solid #fdd005ff",
-      padding: "10px",
-      marginBottom: "10px",
-      borderRadius: "8px"
-    }}>
+    <div className="student-card">
       <h3>Học sinh: {props.name}</h3>
       <p>Tuổi: {props.age}</p>
       <p>Lớp: {props.className}</p>
